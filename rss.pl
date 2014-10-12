02b6a52b8e4e0f4a3c01819ad72853dabd45498d
Athanasius <code@miggy.org>
2014-10-12 19:18:50 +0100

    Starting work on RSS output.

---
+++ rss.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+# vim: textwidth=0 wrapmargin=0 shiftwidth=2 tabstop=2 expandtab softtabstop
+
+use strict;
+use Data::Dumper;
+
+use ED::DevTracker::DB;
+
+my $db = new ED::DevTracker::DB;
