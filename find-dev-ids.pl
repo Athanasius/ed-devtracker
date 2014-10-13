a78f008676b82d361d714cdf180224b5d03e39ee
Athanasius <code@miggy.org>
2014-10-13 09:51:27 +0100

    Bail on finding new IDs if we've reached, and checked, the 'latest user' shown on the forum front page.

--- find-dev-ids.pl
+++ find-dev-ids.pl
@@ -26,21 +26,56 @@ my %uninteresting = (
   'Former Frontier Employee' => 1
 );
 
+my $req = HTTP::Request->new('GET', 'http://forums.frontier.co.uk/index.php');
+my $res = $ua->request($req);
+if (! $res->is_success) {
+  print STDERR "\nFailed to retrieve forum front page\n";
+  exit(1);
+}
+my $tree = HTML::TreeBuilder->new;
+$tree->parse($res->decoded_content);
+$tree->eof();
+$tree->elementify();
+my $stats = $tree->look_down(_tag => 'tbody', id => 'collapseobj_forumhome_stats');
+if (!$stats) {
+  print STDERR "\nCouldn't find the tbody 'collapseobj_forumhome_stats' on front page\n";
+  exit(2);
+}
+my @divs = $stats->look_down(_tag => 'div');
+if (!defined($divs[2])) {
+  print STDERR "\nCouldn't find the 3rd div uner the stats tbody\n";
+  exit(3);
+}
+my $a = $divs[2]->look_down(_tag => 'a');
+if (!$a) {
+  print STDERR "\nCouldn't find the href under the 3rd div\n";
+  exit(4);
+}
+my $latest_url = $a->attr('href');
+$latest_url =~ s/\?s=[^\&]+\&/\?/;
+if ($latest_url !~ /^member\.php\?u=(?<uid>[0-9]+)$/) {
+  print STDERR "\nCouldn't find ID in latest member URL\n";
+  exit(5);
+}
+my $latest_id = $+{'uid'};
+undef $tree;
+
 select STDOUT;
 $| = 1;
-my $id = 50077;
-while ($id <= 51000) {
+my $id = 50050;
+while ($id <= $latest_id) {
   print STDERR "$id, ";
-	my $req = HTTP::Request->new('GET', $url . $id);
-	my $res = $ua->request($req);
+	$req = HTTP::Request->new('GET', $url . $id);
+	$res = $ua->request($req);
 	if (! $res->is_success) {
     print STDERR "\nFailed to retrieve ID: ", $id, "\n";
     $id++;
     next;
   }
-  my $tree = HTML::TreeBuilder->new;
+  $tree = HTML::TreeBuilder->new;
   $tree->parse($res->decoded_content);
   $tree->eof();
+  $tree->elementify();
   my $username_box = $tree->look_down(_tag => 'td', 'id' => 'username_box');
   if (!$username_box) {
     print STDERR "\nFailed to find 'username_box' for: ", $id, "\n";
