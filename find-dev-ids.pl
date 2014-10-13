4752a4ac55ebe68efaf1ed42f6560007e8c47c3b
Athanasius <code@miggy.org>
2014-10-13 09:33:18 +0100

    Should have all the FD forum IDs for now.  Some tweaking to script.

--- find-dev-ids.pl
+++ find-dev-ids.pl
@@ -21,17 +21,20 @@ my %uninteresting = (
   'Deadly' => 1,
   'Elite' => 1,
   'Banned' => 1,
-  'Moderator' => 1
+  'Moderator' => 1,
+  'International Moderator' => 1,
+  'Former Frontier Employee' => 1
 );
 
 select STDOUT;
 $| = 1;
-my $id = 23910;
-while ($id < 51000) {
+my $id = 50077;
+while ($id <= 51000) {
+  print STDERR "$id, ";
 	my $req = HTTP::Request->new('GET', $url . $id);
 	my $res = $ua->request($req);
 	if (! $res->is_success) {
-    print STDERR "Failed to retrieve ID: ", $id, "\n";
+    print STDERR "\nFailed to retrieve ID: ", $id, "\n";
     $id++;
     next;
   }
@@ -40,13 +43,13 @@ while ($id < 51000) {
   $tree->eof();
   my $username_box = $tree->look_down(_tag => 'td', 'id' => 'username_box');
   if (!$username_box) {
-    print STDERR "Failed to find 'username_box' for: ", $id, "\n";
+    print STDERR "\nFailed to find 'username_box' for: ", $id, "\n";
     $id++;
     next;
   }
   my $title = $username_box->look_down(_tag => 'h2');
   if (!$title) {
-    print STDERR "Failed to find 'title' H2 for: ", $id, "\n";
+    print STDERR "\nFailed to find 'title' H2 for: ", $id, "\n";
     $id++;
     next;
   }
@@ -54,11 +57,11 @@ while ($id < 51000) {
     if (!defined($titles{$title->as_text})) {
       $titles{$title->as_text} = $id;
     }
-    print $id, ": ", $title->as_text, "\n";
+    print "\n", $id, ": ", $title->as_text, "\n";
   }
   $id++;
 }
-print "New titles found:\n";
+print "\nNew titles found:\n";
 foreach my $t (sort(keys(%titles))) {
   print $titles{$t}, ": ", $t, "\n";
 }
