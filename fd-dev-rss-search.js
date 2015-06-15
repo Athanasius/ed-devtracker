00db853d95d4dbf42065c0a9b3604db78e65b5a9
Athanasius <code@miggy.org>
2015-06-15 23:15:30 +0100

    Now working via AJAX.

---
+++ fd-dev-rss-search.js
@@ -0,0 +1,100 @@
+/* vim: textwidth=0 wrapmargin=0 shiftwidth=2 tabstop=2 expandtab softtabstop
+ */
+
+var performSearch_inprogress = false;
+
+function performSearch(ev) {
+  ev.preventDefault();
+
+  if (performSearch_inprogress) {
+    $(".results#status").text("");
+    $(".results#status").append("<p>Search is <strong>already</strong> in progress, please <em>be patient</em>!</p>");
+    return;
+  }
+
+  clearResultsOutput();
+
+  if (!$('#search_text').val()) {
+    $(".results#status").text("");
+    $(".results#status").append("<p>You need to enter some search terms!</p>");
+
+    return false;
+  }
+
+  search_query = {
+    search_text: $('#search_text').val()
+  };
+
+  $.ajax({
+    url: "search.pl",
+    type: "GET",
+    data: search_query,
+    dataType: "json",
+    async: true,
+    success: onSearchResults,
+    error: onSearchError,
+    timeout: 20000
+  });
+
+  performSearch_inprogress = true;
+
+  return false;
+}
+
+function onSearchError(jqXHR, textStatus, errorThrown) {
+  console.log('onSearchError: %o, %o, %o', jqXHR, textStatus, errorThrown);
+  performSearch_inprogress = false;
+  $(".results#status").text("");
+  $(".results#status").append("<p>Error performing search: " + textStatus + "(" + errorThrown.message + ")</p>");
+}
+
+function onSearchResults(data) {
+  console.log('onSearchResults: data = %o', data);
+  performSearch_inprogress = false;
+  $(".results#status").text("");
+
+  if (data.status != 'ok') {
+    $(".results#status").append("<p>Search failed: " + data.reason + "</p>");
+    return;
+  }
+
+  displaySearchResults(data);
+}
+
+function displaySearchResults(data) {
+  console.log('displaySearchResults: For array %o', data.results);
+  var after;
+
+  for (r = 0; r < data.results.length ; r++) {
+    var new_r = $("#result_X.result_row").clone();
+    new_r.attr("id", 'result_' + r);
+    console.log('displaySearchResults: cloned search result %o', new_r);
+    if (r == 0) {
+      after = $("#results_output").find("#result_X");
+    }
+    new_r.find(".results_info_item.result_rank").text(data.results[r].rank);
+    new_r.find(".results_info_item.result_datestamp").text(data.results[r].datestamp);
+    // <a href="URL">ThreadTitle</a> (Forum)
+    var new_r_url = new_r.find(".results_info_item.result_thread").html(
+      $('<a>', {
+        href: 'https://forums.frontier.co.uk/' + data.results[r].url,
+        text: data.results[r].threadtitle
+      })
+    );
+    new_r_url.append(' (' + data.results[r].forum + ')');
+    new_r.find(".results_info_item.result_who").text(data.results[r].who);
+    new_r.find(".results_info_item.result_precis").text(data.results[r].precis);
+    new_r.find(".results_info_item.result_matches").html(data.results[r].ts_headline);
+    new_r.insertAfter(after);
+    after = new_r;
+  }
+}
+
+function clearResultsOutput() {
+  $(".results#status").text("");
+  $(".result_row").not("#result_X").remove();
+}
+
+$(function() {
+  $("#form_precis_search").submit(performSearch);
+});
--- search.pl
+++ search.pl
@@ -5,6 +5,7 @@ use strict;
 use Data::Dumper;
 
 use CGI;
+use JSON;
 
 use ED::DevTracker::Config;
 use ED::DevTracker::DB;
@@ -12,64 +13,50 @@ use ED::DevTracker::DB;
 $ENV{'TZ'} = 'UTC';
 my $config = ED::DevTracker::Config->new(file => "config.txt");
 if (!defined($config)) {
-  die "No config!\n";
+  failure("Couldn't find server-side config");
 }
 my $db = new ED::DevTracker::DB('config' => $config);
-
 my $cgi = CGI->new;
-print $cgi->header(-type => "text/html", -charset => "utf-8");
+print $cgi->header(-type => "application/json", -charset => "utf-8");
 if (!defined($cgi->param('search_text'))) {
-  print<<EOHTML;
-<html>
- <body>
-  <p>
-   No search text supplied
-  </p>
- </body>
-</html>
-EOHTML
-  exit(0);
+  failure("No search text supplied");
 }
 
 my $results = $db->precis_ts_search($cgi->param('search_text'));
 if (!defined($results)) {
-  print<<EOHTML;
-<html>
- <body>
-  <p>No results!
-  </p>
- </body>
-</html>
-EOHTML
-  exit(0);
+  failure("No results!");
 }
 
-#print Dumper($results);
-print<<EOHTML;
-<html>
- <body>
-  <table>
-   <tr>
-    <th>Rank</th>
-    <th>Date/time</th>
-    <th>Thread (forum)</th>
-    <th>Poster</th>
-    <th>Precis</th>
-    <th>Matches</th>
-   </tr>
-EOHTML
-foreach my $hit (@{$results}) {
-  print "   <tr>\n";
-  printf "    <td>%8.5f</td>\n", $hit->{'rank'};
-  print  "    <td>", $hit->{'datestamp'}, "</td>\n";
-  print  "    <td><a href=", $hit->{'url'}, ">", $hit->{'threadtitle'}, " (", $hit->{'forum'}, ")</td>\n";
-  print  "    <td>", $hit->{'who'}, "</td>\n";
-  print  "    <td>", $hit->{'precis'}, "</td>\n";
-  print  "    <td>", $hit->{'ts_headline'}, "</td>\n";
-  print "   </tr>\n";
+#my @search_results;
+my %status = ( 'status' => 'ok' );
+#foreach my $hit (@{$results}) {
+#  push(@search_results, {
+#    'rank' => $hit->{'rank'},
+#    'datestamp' => $hit->{'datestamp'},
+#    'url' => $hit->{'url'},
+#    'threadtitle' => $hit->{'threadtitle'},
+#    'forum' => $hit->{'forum'},
+#    'who' => $hit->{'who'},
+#    'precis' => $hit->{'precis'},
+#    'ts_headline' => $hit->{'ts_headline'}
+#  );
+#}
+
+$status{'results'} = $results;
+my $json = to_json(\%status);
+print $json;
+exit(0);
+
+###########################################################################
+# Return Failure
+###########################################################################
+sub failure {
+  my $reason = shift;
+
+  my %status = ( 'status' => 'fail', 'reason' => $reason );
+  my $json = to_json(\%status);
+
+  print $json;
+  exit(0);
 }
-print<<EOHTML;
-  </table>
- </body>
-</html>
-EOHTML
+###########################################################################
