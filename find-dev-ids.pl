ad31ee356f93c2432e57f1b17dfff350966a48b7
Athanasius <code@miggy.org>
2016-09-12 18:44:00 +0100

    find-dev-ids.pl: Updated for current highest number 141829

--- find-dev-ids.pl
+++ find-dev-ids.pl
@@ -104,7 +104,7 @@ undef $tree;
 
 select STDOUT;
 $| = 1;
-my $id = 137770; # STARTID LASTID FIRSTID (No, I can never remember what to search on to get to this line).
+my $id = 141829; # STARTID LASTID FIRSTID (No, I can never remember what to search on to get to this line).
 printf "Scanning from %d to %d\n...", $id, $latest_id;
 while ($id <= $latest_id) {
   print STDERR "$id, ";
