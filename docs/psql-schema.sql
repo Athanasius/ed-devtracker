591de228c391f44371ee45234ed7d309009ddf14
Athanasius <code@miggy.org>
2020-07-16 18:07:35 +0100

    Update posts.urltext to char varying(256)
    128 wasn't enough for a forum thread title.
    
    Also this is dumped using later version of psql.

--- docs/psql-schema.sql
+++ docs/psql-schema.sql
@@ -2,15 +2,17 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.7
--- Dumped by pg_dump version 9.6.7
+-- Dumped from database version 9.6.17
+-- Dumped by pg_dump version 9.6.17
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -28,13 +30,11 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 --
--- Name: ignored_posts_id_seq; Type: SEQUENCE; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: ignored_posts_id_seq; Type: SEQUENCE; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE SEQUENCE ignored_posts_id_seq
+CREATE SEQUENCE public.ignored_posts_id_seq
     START WITH 0
     INCREMENT BY 1
     MINVALUE 0
@@ -42,34 +42,34 @@ CREATE SEQUENCE ignored_posts_id_seq
     CACHE 1;
 
 
-ALTER TABLE ignored_posts_id_seq OWNER TO ed_devtracker_crawl_dev;
+ALTER TABLE public.ignored_posts_id_seq OWNER TO ed_devtracker_crawl;
 
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: ignored_posts; Type: TABLE; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: ignored_posts; Type: TABLE; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE TABLE ignored_posts (
-    id integer DEFAULT nextval('ignored_posts_id_seq'::regclass),
+CREATE TABLE public.ignored_posts (
+    id integer DEFAULT nextval('public.ignored_posts_id_seq'::regclass),
     datestamp timestamp without time zone DEFAULT now(),
     url character varying(512)
 );
 
 
-ALTER TABLE ignored_posts OWNER TO ed_devtracker_crawl_dev;
+ALTER TABLE public.ignored_posts OWNER TO ed_devtracker_crawl;
 
 --
--- Name: posts; Type: TABLE; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts; Type: TABLE; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE TABLE posts (
+CREATE TABLE public.posts (
     id integer NOT NULL,
     datestamp timestamp without time zone,
     url character varying(512),
-    urltext character varying(128),
+    urltext character varying(256),
     threadurl character varying(512),
     threadtitle text,
     forum character varying(64),
@@ -90,13 +90,13 @@ CREATE TABLE posts (
 );
 
 
-ALTER TABLE posts OWNER TO ed_devtracker_crawl_dev;
+ALTER TABLE public.posts OWNER TO ed_devtracker_crawl;
 
 --
--- Name: posts_id; Type: SEQUENCE; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_id; Type: SEQUENCE; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE SEQUENCE posts_id
+CREATE SEQUENCE public.posts_id
     START WITH 0
     INCREMENT BY 1
     MINVALUE 0
@@ -104,92 +104,92 @@ CREATE SEQUENCE posts_id
     CACHE 1;
 
 
-ALTER TABLE posts_id OWNER TO ed_devtracker_crawl_dev;
+ALTER TABLE public.posts_id OWNER TO ed_devtracker_crawl;
 
 --
--- Name: posts_id; Type: SEQUENCE OWNED BY; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_id; Type: SEQUENCE OWNED BY; Schema: public; Owner: ed_devtracker_crawl
 --
 
-ALTER SEQUENCE posts_id OWNED BY posts.id;
-
-
---
--- Name: posts id; Type: DEFAULT; Schema: public; Owner: ed_devtracker_crawl_dev
---
-
-ALTER TABLE ONLY posts ALTER COLUMN id SET DEFAULT nextval('posts_id'::regclass);
+ALTER SEQUENCE public.posts_id OWNED BY public.posts.id;
 
 
 --
--- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts id; Type: DEFAULT; Schema: public; Owner: ed_devtracker_crawl
 --
 
-ALTER TABLE ONLY posts
+ALTER TABLE ONLY public.posts ALTER COLUMN id SET DEFAULT nextval('public.posts_id'::regclass);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: ed_devtracker_crawl
+--
+
+ALTER TABLE ONLY public.posts
     ADD CONSTRAINT posts_pkey PRIMARY KEY (id);
 
 
 --
--- Name: posts posts_url_key; Type: CONSTRAINT; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts posts_url_key; Type: CONSTRAINT; Schema: public; Owner: ed_devtracker_crawl
 --
 
-ALTER TABLE ONLY posts
+ALTER TABLE ONLY public.posts
     ADD CONSTRAINT posts_url_key UNIQUE (url);
 
 
 --
--- Name: posts_datestamp_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_datestamp_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE INDEX posts_datestamp_index ON posts USING btree (datestamp);
-
-
---
--- Name: posts_guid_url_key; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl_dev
---
-
-CREATE UNIQUE INDEX posts_guid_url_key ON posts USING btree (guid_url);
+CREATE INDEX posts_datestamp_index ON public.posts USING btree (datestamp);
 
 
 --
--- Name: posts_precis_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_guid_url_key; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE INDEX posts_precis_index ON posts USING gin (precis_ts_indexed);
-
-
---
--- Name: posts_threadtitle_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl_dev
---
-
-CREATE INDEX posts_threadtitle_index ON posts USING gin (threadtitle_ts_indexed);
+CREATE UNIQUE INDEX posts_guid_url_key ON public.posts USING btree (guid_url);
 
 
 --
--- Name: posts ts_fulltext_noquotes_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_precis_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE TRIGGER ts_fulltext_noquotes_vectorupdate BEFORE INSERT OR UPDATE ON posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('fulltext_noquotes_ts_indexed', 'pg_catalog.english', 'fulltext_noquotes_stripped');
-
-
---
--- Name: posts ts_fulltext_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl_dev
---
-
-CREATE TRIGGER ts_fulltext_vectorupdate BEFORE INSERT OR UPDATE ON posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('fulltext_ts_indexed', 'pg_catalog.english', 'fulltext_stripped');
+CREATE INDEX posts_precis_index ON public.posts USING gin (precis_ts_indexed);
 
 
 --
--- Name: posts ts_precis_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts_threadtitle_index; Type: INDEX; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE TRIGGER ts_precis_vectorupdate BEFORE INSERT OR UPDATE ON posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('precis_ts_indexed', 'pg_catalog.english', 'precis');
+CREATE INDEX posts_threadtitle_index ON public.posts USING gin (threadtitle_ts_indexed);
 
 
 --
--- Name: posts ts_threadtitle_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl_dev
+-- Name: posts ts_fulltext_noquotes_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl
 --
 
-CREATE TRIGGER ts_threadtitle_vectorupdate BEFORE INSERT OR UPDATE ON posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('threadtitle_ts_indexed', 'pg_catalog.english', 'threadtitle');
+CREATE TRIGGER ts_fulltext_noquotes_vectorupdate BEFORE INSERT OR UPDATE ON public.posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('fulltext_noquotes_ts_indexed', 'pg_catalog.english', 'fulltext_noquotes_stripped');
+
+
+--
+-- Name: posts ts_fulltext_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl
+--
+
+CREATE TRIGGER ts_fulltext_vectorupdate BEFORE INSERT OR UPDATE ON public.posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('fulltext_ts_indexed', 'pg_catalog.english', 'fulltext_stripped');
+
+
+--
+-- Name: posts ts_precis_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl
+--
+
+CREATE TRIGGER ts_precis_vectorupdate BEFORE INSERT OR UPDATE ON public.posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('precis_ts_indexed', 'pg_catalog.english', 'precis');
+
+
+--
+-- Name: posts ts_threadtitle_vectorupdate; Type: TRIGGER; Schema: public; Owner: ed_devtracker_crawl
+--
+
+CREATE TRIGGER ts_threadtitle_vectorupdate BEFORE INSERT OR UPDATE ON public.posts FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger('threadtitle_ts_indexed', 'pg_catalog.english', 'threadtitle');
 
 
 --
